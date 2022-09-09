8cd990177720455ad1ae2fc831a41e625340eb66
Petr Katerinak <petrkaterinak@inove.cz>
2022-09-09 06:59:53 +0200

    Updated employee edit validation messages location

--- resources/views/livewire/modals/edit-employee.blade.php
+++ resources/views/livewire/modals/edit-employee.blade.php
@@ -8,31 +8,31 @@
                 <label class="block mt-2 w-1/2">
                     <span class="text-gray-700 text-lg">Jméno</span>
                     <input type="text" wire:model="name" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="">
+                    @error('name') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
                 </label>
-                @error('name') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
 
                 <label class="block mt-2 w-1/2">
                     <span class="text-gray-700 text-lg">Příjmení</span>
                     <input type="text" wire:model="surname" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="">
+                    @error('surname') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
                 </label>
-                @error('surname') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
             </div>
 
             <div class="flex space-x-4">
                 <label class="block mt-2 w-1/2">
                     <span class="text-gray-700 text-lg">Datum</span>
                     <input type="date" wire:model="date" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="">
+                    @error('date') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
                 </label>
-                @error('date') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
 
                 <div class="block mt-2 w-1/2 relative">
                     <span class="text-gray-700 text-lg">Výplata</span>
                     <div class="relative">
                         <input type="number" wire:model="salary" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="">
                         <span class="absolute top-1/2 right-6 transform -translate-x-1/2 -translate-y-1/2 text-gray-700 origin-center">Kč</span>
+                        @error('salary') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
                     </div>
                 </div>
-                @error('salary') <div class="mt-1 text-xs text-red-500">{{ $message }}</div> @enderror
             </div>
         </div>
         <div class="w-full px-4 py-2 bg-blue-500/10 flex justify-end space-x-4">
